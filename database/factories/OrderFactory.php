140bf7c5bbf3c24a0cd653d149fb247d56804a70
mohammed Abd Aljwad <66750711+dev-mohammed-abdaljwad@users.noreply.github.com>
2025-12-03 20:46:57 +0200

    Create OrderFactory.php

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        'product_id' => Product::factory(),
+            'quantity' => 1,
+            'total_amount' => 15000,
+            'payment_status' => 'pending',
+            'order_status' => 'processing',
+        ];
+    }
+}
